1a89e59ca5a723cee15222f63b7d7fb0edded505
Aleksandar Dmitrovic <aleksandar.dmitrovic@gmail.com>
2020-09-22 19:41:31 +0000

    Added new line character

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,10 @@
 let timer = 100;
-let spinner = ['|', '/', '-' , '\\', '|', '/', '-' , '\\', '|' ];
+let spinner = ['|', '/', '-' , '\\', '|', '/', '-' , '\\', '|', '\n'];
 
 for (const spin of spinner) {
   setTimeout(() => {
     process.stdout.write(`\r${spin}  `)
   },timer);
   timer += 200;
+  console.log('timer :', timer);
 }
